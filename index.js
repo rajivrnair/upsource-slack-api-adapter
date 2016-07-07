60d939d6c21275986dbc79fd0109c79d73aeaae7
Varun V Subramanian <varunvsv@gmail.com>
2016-07-07 16:54:52 +0530

    Add NewRevisionCreated adapter

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 var express = require('express');
-var app = express();
+var server = express();
 var bodyParser = require('body-parser');
 var util = require('util');
-var MessageFactory = 
+var app = require('./app');
 
-app.use(bodyParser.json());
+server.use(bodyParser.json());
 
-app.post('/', function (req, res) {
+server.post('/', function (req, res) {
+  app.talkToSlack(req.body);
   console.log(util.inspect(req.body, false, null));
   res.end();
 });
 
-app.listen(4000, function() {
+server.listen(4000, function() {
   console.log('The app has started and is running on :4000');
 });
 
