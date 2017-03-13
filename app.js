5965d1f124c78e77570dd53f5d7a407ab31dcb0b
Grzegorz Nowak <strange3studio@gmail.com>
2017-03-13 16:48:30 +0100

    remove dependency from Maybe, do not autoprefix with #, but default to it if neither # nor @ is present for channel name

--- app.js
+++ app.js
@@ -2,21 +2,27 @@ const config   = require('./config.json');
 const axios    = require('axios');
 const Adapters = require('./adapters');
 const _        = require('lodash');
-const Maybe    = require('maybe');
 
 module.exports = {
 	talkToSlack: function(review, query) {
 
 		const adapter = Adapters[review.dataType];
-		var channel   = Maybe(query.channel);
 
 		if (_.isUndefined(adapter)) {
 			console.log(`No adapter has been registered for the event ${review.dataType}`);
 			return;
 		}
 		var adapted = adapter(review);
-		if(channel.isJust()) {
-			adapted.channel = "#"+channel.value()
+		if(!_.isUndefined(query.channel)) {
+
+			// be compatible with parameters without a prefix. In which case we default to a general channel
+			var   prefix         = '';
+			const firstCharacter = query.channel.charAt(0);
+
+			if(firstCharacter != '#' && firstCharacter != '@') {
+				prefix = '#';
+			}
+			adapted.channel = prefix+query.channel;
 		}
 		axios.post(config.slackWebhookUrl, adapted);
 	}
