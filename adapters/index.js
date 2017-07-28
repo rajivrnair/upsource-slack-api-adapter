f88ae1994dc598459eb1372a48dc9a9d3eb134ba
Raman Gupta <rocketraman@gmail.com>
2017-07-28 13:54:37 -0400

    Better participant add messaging, support remove

--- adapters/index.js
+++ adapters/index.js
@@ -2,6 +2,7 @@ module.exports = {
 	'ReviewCreatedFeedEventBean': require('./review-created-feed-event'),
 	'ReviewStateChangedFeedEventBean': require('./review-state-changed-feed-event'),
 	'ParticipantStateChangedFeedEventBean': require('./participant-state-changed-feed-event'),
-	'NewParticipantInReviewFeedEventBean': require('./new-participant-in-review-feed-event'),
+	'NewParticipantInReviewFeedEventBean': require('./participant-in-review-feed-event'),
+	'RemovedParticipantFromReviewFeedEventBean': require('./participant-in-review-feed-event'),
 	'DiscussionFeedEventBean': require('./discussion-feed-event')
 };
