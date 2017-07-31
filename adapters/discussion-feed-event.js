9c5e21de3664488a363e6984c0dd6def973c7baf
Raman Gupta <rocketraman@gmail.com>
2017-07-31 18:30:30 -0400

    Use Upsource API to get/show review title
    Use the Upsource API to get the review title and show it in the Slack
    message board. The review title provides very useful context to Slack
    users, which is otherwise missing. Unfortunately the title is not
    available in the event bean, so a new request has to be made to obtain
    it.
    
    Many optimizations and improvements are possible:
    
    * caching to avoid repeated requests for the same info
    * use of review details in other ways e.g. differentiate the types of
    participants

--- adapters/discussion-feed-event.js
+++ adapters/discussion-feed-event.js
@@ -1,36 +1,57 @@
 const _ = require('lodash');
+const request = require('request');
 const config = require('../config.json');
 
 module.exports = function(review, channel) {
+	"use strict";
 	const reviewers = _.chain(review).get('data.base.userIds', []).map('userName').value().join(', ');
 
-	return {
-		text: `Review #${review.data.base.reviewNumber}: New comment by *${_.get(review, 'data.base.actor.userName', '')}*`,
-		attachments: [
-			{
-				fallback: `Review #${review.data.base.reviewNumber}: New comment`,
-				fields: [
-					{
-						title: 'Project',
-						value: review.projectId,
-						short: true
-					},
-					{
-						title: 'Reviewer(s)',
-						value: reviewers,
-						short: true
-					},
-					{
-						title: 'Comment',
-						value: review.data.commentText
-					},
-					{
-						title: 'link',
-						value: '<' + config.upsourceUrl + '/' + review.projectId + '/review/' + review.data.base.reviewId + '>'
-					}
-				],
-				color: '#3AA3E3'
+	return new Promise(function(resolve, reject) {
+		request.post(config.upsourceUrl + "/~rpc/getReviewDetails", {
+			auth: {
+				"user": config.upsourceUser,
+				"pass": config.upsourcePass,
+				"sendImmediately": true
+			},
+			json: true,
+			body: {
+				"projectId": review.projectId,
+				"reviewId": review.data.base.reviewId
 			}
-		]
-	}
+		}).on('error', function(err) {
+			reject(err);
+		})
+		.on('data', function(body) {
+			let title = JSON.parse(body).result.title;
+			resolve({
+				text: `Review *${title}* (${review.data.base.reviewId}): New comment by *${_.get(review, 'data.base.actor.userName', '')}*`,
+				attachments: [
+					{
+						fallback: `Review *${title}* (${review.data.base.reviewId}): New comment`,
+						fields: [
+							{
+								title: 'Project',
+								value: review.projectId,
+								short: true
+							},
+							{
+								title: 'Participant(s)',
+								value: reviewers,
+								short: true
+							},
+							{
+								title: 'Comment',
+								value: review.data.commentText
+							},
+							{
+								title: 'link',
+								value: '<' + config.upsourceUrl + '/' + review.projectId + '/review/' + review.data.base.reviewId + '>'
+							}
+						],
+						color: '#3AA3E3'
+					}
+				]
+			});
+		});
+	});
 };
--- adapters/participant-in-review-feed-event.js
+++ adapters/participant-in-review-feed-event.js
@@ -1,4 +1,5 @@
 const _ = require('lodash');
+const request = require('request');
 const config = require('../config.json');
 
 module.exports = function(review) {
@@ -10,94 +11,112 @@ module.exports = function(review) {
 		3: '_Watcher_'
 	};
 
-	if(review.dataType === "RemovedParticipantFromReviewFeedEventBean") {
-		const formerRole = review.data.formerRole;
-		let participant = review.data.participant.userName;
-		if(participant === undefined) {
-			participant = review.data.participant.userId;
-		}
-		return {
-			text: `Review #${review.data.base.reviewNumber}: ${participant} is no longer a ${reviewState[formerRole]}`,
-			attachments: [
-				{
-					fallback: `Review #${review.data.base.reviewNumber}: Participants changed`,
-					fields: [
-						{
-							title: 'Project',
-							value: review.projectId,
-							short: true
-						},
-						{
-							title: 'Participant(s)',
-							value: reviewers,
-							short: true
-						},
-						{
-							title: 'link',
-							value: '<' + config.upsourceUrl + '/' + review.projectId + '/review/' + review.data.base.reviewId + '>'
-						}
-					],
-					color: '#2AB27B'
+	return new Promise(function(resolve, reject) {
+		request.post(config.upsourceUrl + "/~rpc/getReviewDetails", {
+			auth: {
+				"user": config.upsourceUser,
+				"pass": config.upsourcePass,
+				"sendImmediately": true
+			},
+			json: true,
+			body: {
+				"projectId": review.projectId,
+				"reviewId": review.data.base.reviewId
+			}
+		}).on('error', function(err) {
+			reject(err);
+		})
+		.on('data', function(body) {
+			let title = JSON.parse(body).result.title;
+			if(review.dataType === "RemovedParticipantFromReviewFeedEventBean") {
+				const formerRole = review.data.formerRole;
+				let participant = review.data.participant.userName;
+				if(participant === undefined) {
+					participant = review.data.participant.userId;
 				}
-			]
-		}
-	} else if (review.dataType === "NewParticipantInReviewFeedEventBean") {
-		const role = review.data.role;
-		let participant = review.data.participant.userName;
-		if(participant === undefined) {
-			participant = review.data.participant.userId;
-		}
-		return {
-			text: `Review #${review.data.base.reviewNumber}: ${participant} is now a ${reviewState[role]}`,
-			attachments: [
-				{
-					fallback: `Review #${review.data.base.reviewNumber}: Participants changed`,
-					fields: [
+				resolve({
+					text: `Review *${title}* (${review.data.base.reviewId}): ${participant} is no longer a ${reviewState[formerRole]}`,
+					attachments: [
 						{
-							title: 'Project',
-							value: review.projectId,
-							short: true
-						},
-						{
-							title: 'Participant(s)',
-							value: reviewers,
-							short: true
-						},
-						{
-							title: 'link',
-							value: '<' + config.upsourceUrl + '/' + review.projectId + '/review/' + review.data.base.reviewId + '>'
+							fallback: `Review *${title}* (${review.data.base.reviewId}): Participants changed`,
+							fields: [
+								{
+									title: 'Project',
+									value: review.projectId,
+									short: true
+								},
+								{
+									title: 'Participant(s)',
+									value: reviewers,
+									short: true
+								},
+								{
+									title: 'link',
+									value: '<' + config.upsourceUrl + '/' + review.projectId + '/review/' + review.data.base.reviewId + '>'
+								}
+							],
+							color: '#2AB27B'
 						}
-					],
-					color: '#2AB27B'
+					]
+				});
+			} else if (review.dataType === "NewParticipantInReviewFeedEventBean") {
+				const role = review.data.role;
+				let participant = review.data.participant.userName;
+				if(participant === undefined) {
+					participant = review.data.participant.userId;
 				}
-			]
-		}
-	} else {
-		return {
-			text: `Review #${review.data.base.reviewNumber}: Participants changed`,
-			attachments: [
-				{
-					fallback: `Review #${review.data.base.reviewNumber}: Participants changed`,
-					fields: [
+				resolve({
+					text: `Review *${title}* (${review.data.base.reviewId}): ${participant} is now a ${reviewState[role]}`,
+					attachments: [
 						{
-							title: 'Project',
-							value: review.projectId,
-							short: true
-						},
-						{
-							title: 'Participant(s)',
-							value: reviewers,
-							short: true
-						},
-						{
-							title: 'link',
-							value: '<' + config.upsourceUrl + '/' + review.projectId + '/review/' + review.data.base.reviewId + '>'
+							fallback: `Review *${title}* (${review.data.base.reviewId}): Participants changed`,
+							fields: [
+								{
+									title: 'Project',
+									value: review.projectId,
+									short: true
+								},
+								{
+									title: 'Participant(s)',
+									value: reviewers,
+									short: true
+								},
+								{
+									title: 'link',
+									value: '<' + config.upsourceUrl + '/' + review.projectId + '/review/' + review.data.base.reviewId + '>'
+								}
+							],
+							color: '#2AB27B'
 						}
-					],
-					color: '#2AB27B'
-				}
-			]
-		}
-	}
-
+					]
+				});
+			} else {
+				resolve({
+					text: `Review *${title}* (${review.data.base.reviewId}): Participants changed`,
+					attachments: [
+						{
+							fallback: `Review *${title}* (${review.data.base.reviewId}): Participants changed`,
+							fields: [
+								{
+									title: 'Project',
+									value: review.projectId,
+									short: true
+								},
+								{
+									title: 'Participant(s)',
+									value: reviewers,
+									short: true
+								},
+								{
+									title: 'link',
+									value: '<' + config.upsourceUrl + '/' + review.projectId + '/review/' + review.data.base.reviewId + '>'
+								}
+							],
+							color: '#2AB27B'
+						}
+					]
+				});
+			}
+		});
+    });
 };
